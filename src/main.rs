f18605c055a0ff830f18d1331cf2460da0062490
zimingYS <1563238589@qq.com>
2024-12-03 18:10:44 +0800

    初始化窗口

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+////项目说明:
+////本项目实现使用Rust WGPU图像库编写一个康威生命游戏
+////代码逻辑参考自https://linux.cn/article-8933-1.html
+////此项目遵循GPL-v3.0进行开源
+////如果此项目涉及侵权，请联系作者或在讨论中提出
+////仅作学习探讨使用
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+use wgpu::*;
+
 fn main() {
-    println!("Hello, world!");
+    //初始化窗口
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    //窗口事件循环
+    event_loop.run(move |event, _, control_flow|match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event{
+            WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput{
+                        state: ElementState::Pressed,
+                        virtual_keycode:Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            }  => {
+                *control_flow = ControlFlow::Exit
+            }
+            _ => {}
+        },
+        _ => {}
+    });
 }
